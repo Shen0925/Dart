2f40d8aaa507b1f8f27a31bd1489a899248adf92
Shen0925 <142953087+Shen0925@users.noreply.github.com>
2024-06-06 16:34:28 +0800

    Create variables.dart

---
+++ variables.dart
@@ -0,0 +1,8 @@
+var university = 'Yunnan University';
+var studyYear = 2023;
+var major = 'Computer Science';
+var courses = ['Data Structures', 'Operating Systems', 'Machine Learning', 'Software Engineering'];
+var project = {
+  'tags': ['final year', 'thesis'],
+  'url': '//path/to/final-year-project.jpg'
+};
